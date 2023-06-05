77bb346d27a1dc6b4e7dfa13375e365f70fc35eb
aminespinoza10 <amin.espinoza@outlook.com>
2023-06-05 18:58:17 -0500

    go service added

--- src/go/main.go
+++ src/go/main.go
@@ -1,38 +1,21 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
+    "fmt"
+    "html"
+    "log"
+    "net/http"
 )
 
-var name string
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.html"))
-}
-
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/favicion", favicon)
-	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
-	http.ListenAndServe(":8080", nil)
-}
 
-func index(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		name = req.FormValue("name")
-		http.Redirect(w, req, "/hello", http.StatusSeeOther)
-		return
-	}
-	tpl.ExecuteTemplate(w, "index.html", nil)
-}
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "hello.html", name)
-}
+    http.HandleFunc("/WeatherForecast", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Today is gonna be a great cloudy day for a go service!")
+    })
 
-func favicon(w http.ResponseWriter, req *http.Request) {
-	http.ServeFile(w, req, "/favicon.ico")
+    log.Fatal(http.ListenAndServe(":8081", nil))
 }
